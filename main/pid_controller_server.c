6cc692315857f2a97d15906a9a8d71f49af9f55b
ussserrr <andrei4.2008@gmail.com>
2018-12-15 03:28:29 +0300

    Prepare the main for PID integration

--- main/pid_controller_server.c
+++ main/pid_controller_server.c
@@ -1,11 +1,6 @@
-/* BSD Socket API Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/*
+ *  PID controller server
+ */
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -22,21 +17,22 @@
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
-// hide personal data from repository
-#include "../../my_wifi.h"
+#include "../../my_wifi.h"  // hide personal data from repository
 
 
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
+/*
+ *  From my_wifi.h
+ */
 #define EXAMPLE_WIFI_SSID MY_SSID
 #define EXAMPLE_WIFI_PASS MY_PSWD
 
-#define PORT CONFIG_EXAMPLE_PORT
+#define UDP_PORT 3333
 
-/* FreeRTOS event group to signal when we are connected & ready to make a request */
+
+
+/*
+ *  FreeRTOS event group to signal when we are connected & ready to make a request
+ */
 static EventGroupHandle_t wifi_event_group;
 
 const int IPV4_GOTIP_BIT = BIT0;
@@ -44,8 +40,8 @@ const int IPV6_GOTIP_BIT = BIT1;
 
 static const char *TAG = "pid-controller-server";
 
-static esp_err_t event_handler(void *ctx, system_event_t *event)
-{
+static esp_err_t event_handler(void *ctx, system_event_t *event) {
+
     switch (event->event_id) {
     case SYSTEM_EVENT_STA_START:
         esp_wifi_connect();
@@ -60,7 +56,7 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
         ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
-        /* This is a workaround as ESP32 WiFi libs don't currently auto-reassociate. */
+        /* This is a workaround as ESP32 WiFi libs don't currently auto-reassociate */
         esp_wifi_connect();
         xEventGroupClearBits(wifi_event_group, IPV4_GOTIP_BIT);
         xEventGroupClearBits(wifi_event_group, IPV6_GOTIP_BIT);
@@ -68,58 +64,32 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     case SYSTEM_EVENT_AP_STA_GOT_IP6:
         xEventGroupSetBits(wifi_event_group, IPV6_GOTIP_BIT);
         ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP6");
-
         char *ip6 = ip6addr_ntoa(&event->event_info.got_ip6.ip6_info.ip);
         ESP_LOGI(TAG, "IPv6: %s", ip6);
     default:
         break;
     }
+
     return ESP_OK;
 }
 
-static void initialise_wifi(void)
-{
-    tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_WIFI_SSID,
-            .password = EXAMPLE_WIFI_PASS,
-        },
-    };
-    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-}
 
-static void wait_for_ip()
-{
-    uint32_t bits = IPV4_GOTIP_BIT | IPV6_GOTIP_BIT ;
 
-    ESP_LOGI(TAG, "Waiting for AP connection...");
-    xEventGroupWaitBits(wifi_event_group, bits, false, true, portMAX_DELAY);
-    ESP_LOGI(TAG, "Connected to AP");
-}
-
-static void udp_server_task(void *pvParameters)
-{
+static void udp_server_task(void *pvParameters) {
+    
     char rx_buffer[128];
     char addr_str[128];
     int addr_family;
     int ip_protocol;
 
+
     while (1) {
 
-#ifdef CONFIG_EXAMPLE_IPV4
+#ifdef CONFIG_IPV4
         struct sockaddr_in destAddr;
         destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
         destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(PORT);
+        destAddr.sin_port = htons(UDP_PORT);
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
         inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
@@ -127,7 +97,7 @@ static void udp_server_task(void *pvParameters)
         struct sockaddr_in6 destAddr;
         bzero(&destAddr.sin6_addr.un, sizeof(destAddr.sin6_addr.un));
         destAddr.sin6_family = AF_INET6;
-        destAddr.sin6_port = htons(PORT);
+        destAddr.sin6_port = htons(UDP_PORT);
         addr_family = AF_INET6;
         ip_protocol = IPPROTO_IPV6;
         inet6_ntoa_r(destAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
@@ -146,28 +116,30 @@ static void udp_server_task(void *pvParameters)
         }
         ESP_LOGI(TAG, "Socket binded");
 
+
         while (1) {
 
             ESP_LOGI(TAG, "Waiting for data");
-            struct sockaddr_in6 sourceAddr; // Large enough for both IPv4 or IPv6
+            struct sockaddr_in6 sourceAddr; // large enough for both IPv4 or IPv6
             socklen_t socklen = sizeof(sourceAddr);
             int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&sourceAddr, &socklen);
 
-            // Error occured during receiving
+            // error occured during receiving
             if (len < 0) {
                 ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
                 break;
             }
-            // Data received
+            // data received
             else {
-                // Get the sender's ip address as string
+                // get the sender's ip address as string
                 if (sourceAddr.sin6_family == PF_INET) {
                     inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
-                } else if (sourceAddr.sin6_family == PF_INET6) {
+                }
+                else if (sourceAddr.sin6_family == PF_INET6) {
                     inet6_ntoa_r(sourceAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
                 }
 
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+                rx_buffer[len] = 0; // null-terminate whatever we received and treat like a string...
                 ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
                 ESP_LOGI(TAG, "%s", rx_buffer);
 
@@ -185,14 +157,47 @@ static void udp_server_task(void *pvParameters)
             close(sock);
         }
     }
+
+
     vTaskDelete(NULL);
 }
 
-void app_main()
-{
+
+
+void app_main() {
+
     ESP_ERROR_CHECK( nvs_flash_init() );
-    initialise_wifi();
-    wait_for_ip();
+
+
+    /*
+     *  Initialize Wi-Fi
+     */
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_WIFI_SSID,
+            .password = EXAMPLE_WIFI_PASS,
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+
+
+    /*
+     *  Wait for IP
+     */
+    uint32_t bits = IPV4_GOTIP_BIT | IPV6_GOTIP_BIT ;
+    ESP_LOGI(TAG, "Waiting for AP connection...");
+    xEventGroupWaitBits(wifi_event_group, bits, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to AP");
+
 
     xTaskCreate(udp_server_task, "udp_server", 4096, NULL, 5, NULL);
 }
